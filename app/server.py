9501a91e137f964c7bb4b6ea00e4607156e24c3b
Yann Klein <yann.klein@me.com>
2021-06-17 12:45:54 +0900

    added nest asyncio

--- app/server.py
+++ app/server.py
@@ -9,6 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+import nest_asyncio
+nest_asyncio.apply()
+print('nest async up!')
+
 # Base example model
 # export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 # export_file_name = 'export.pkl'
