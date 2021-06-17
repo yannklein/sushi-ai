c7d01a09484eb2666756dda2aec398f4cda5282f
Yann Klein <yann.klein@me.com>
2021-06-17 13:27:45 +0900

    put CNN back in place

--- app/server.py
+++ app/server.py
@@ -108,10 +108,10 @@ async def setup_learner():
             raise
 
 
-# loop = asyncio.get_event_loop()
-# tasks = [asyncio.ensure_future(setup_learner())]
-# learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
-# loop.close()
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
 
 
 @app.route('/')
